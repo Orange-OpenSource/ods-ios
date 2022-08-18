987db0b2a1137e54863e3110de858028d8a10d60
Ludovic PINEL <ludovic.pinel@orange.com>
2022-08-18 14:42:43 +0200

    Refactoring 1

--- Package.swift
+++ Package.swift
@@ -15,16 +15,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        
+
     ],
-    
+
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OrangeDesignSystem",
             dependencies: [],
-            path: "OrangeDesignSystem/Sources"
-        )
-    ]
-)
+            path: "OrangeDesignSystem/Sources",
+            resources: [.copy("Assets/Colors.xcassets"),
+                        .copy("Assets/Icons.xcassets"),
+                        .copy("Assets/Images.xcassets")]),
+    ])
