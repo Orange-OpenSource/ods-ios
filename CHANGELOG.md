95e364984bb3c6bd3f8597b1ec03a05ad673d919
Jean-Luc Chevalier <jl.chevalier@orange.com>
2022-08-23 15:03:59 +0200

    replace text selection component by text field (only demo) (#201)
    * replace text selection component by text field (only demo)
    * use same padding for bottom in all demo pages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+- [DemoApp] #160 Update the Text Field page
 - [DemoApp] #181 Update Slider Voice Over Accessibility  
 - [DemoApp/SDK] #198 Update ODSChipPicker API to seperate zero or one element in the single selection 
 - [DemoApp/SDK] #199 Refactor sources
--- OrangeDesignSystemDemo/OrangeDesignSystemDemo/Views/Components/Pages/ShapeButtonPage.swift
+++ OrangeDesignSystemDemo/OrangeDesignSystemDemo/Views/Components/Pages/ShapeButtonPage.swift
@@ -38,7 +38,7 @@ struct ShapeButtonPage: View {
                 VariantsTitle()
                 ButtonList().padding(.bottom, ODSSpacing.s)
             }
-            .padding(EdgeInsets(top: ODSSpacing.none, leading: ODSSpacing.m, bottom: ODSSpacing.xs, trailing: ODSSpacing.m))
+            .padding(EdgeInsets(top: ODSSpacing.none, leading: ODSSpacing.m, bottom: ODSSpacing.m, trailing: ODSSpacing.m))
         }
         .background(ODSColor.primaryBackground.color)
     }
