ce8400aa7e7be7e8fb047be1544a1b152a5afb8d
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2023-10-23 15:54:58 +0200

    feat(#466): l10n support for progress bars page (demo)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OrangeDesignSystemDemo/OrangeDesignSystemDemo/Screens/Components/Pages/ProgressIndicator/ProgressBarVariant.swift
+++ OrangeDesignSystemDemo/OrangeDesignSystemDemo/Screens/Components/Pages/ProgressIndicator/ProgressBarVariant.swift
@@ -1,25 +1,10 @@
-//
-// MIT License
-// Copyright (c) 2021 Orange
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the  Software), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-//
+/*
+ * Software Name: Orange Design System (iOS)
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2023 Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license.
+ */
 
 import OrangeDesignSystem
 import SwiftUI
@@ -55,7 +40,7 @@ struct ProgressBarVariant: View {
         VStack {
             ProgressView(value: secondsElapsed, total: maxSeconds) {
                 if model.showLabel {
-                    Label(title: {Text("Downloading...")}, icon: {
+                    Label(title: {Text("screens.guidelines.progress_indicators.downloading")}, icon: {
                         if model.showIconInLabel {
                             Image(systemName: "tray.and.arrow.down")
                         }
@@ -64,7 +49,7 @@ struct ProgressBarVariant: View {
             } currentValueLabel: {
                 if model.showCurrentValue {
                     let percent = String(format: "%.0f", secondsElapsed)
-                    Text("\(percent) %")
+                    Text("screens.guidelines.progress_indicators.x_percent" <- percent)
                         .frame(maxWidth: .infinity, alignment: .trailing)
                 }
             }
@@ -118,11 +103,11 @@ private struct ProgressBarVariantOptions: View {
 
     var body: some View {
         VStack(spacing: ODSSpacing.m) {
-            Toggle("Label", isOn: $model.showLabel)
+            Toggle("screens.guidelines.progress_indicators.toggle.label", isOn: $model.showLabel)
             if model.showLabel {
-                Toggle("Icon", isOn: $model.showIconInLabel)
+                Toggle("screens.guidelines.progress_indicators.toggle.icon", isOn: $model.showIconInLabel)
             }
-            Toggle("Current value", isOn: $model.showCurrentValue)
+            Toggle("screens.guidelines.progress_indicators.toggle.current_value", isOn: $model.showCurrentValue)
         }
         .odsFont(.bodyRegular)
         .padding(.all, ODSSpacing.m)
