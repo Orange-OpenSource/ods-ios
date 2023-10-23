a308bc93765c294f8de89030a6765e2a3b5b7c3a
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2023-10-23 16:57:27 +0200

    feat(#466): l10n support for sliders page (demo)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OrangeDesignSystemDemo/OrangeDesignSystemDemo/Screens/Components/Pages/Sliders/SlidersVariant.swift
+++ OrangeDesignSystemDemo/OrangeDesignSystemDemo/Screens/Components/Pages/Sliders/SlidersVariant.swift
@@ -1,25 +1,10 @@
-//
-// MIT License
-// Copyright (c) 2021 Orange
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the  Software), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
-//
+/*
+ * Software Name: Orange Design System (iOS)
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2023 Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license.
+ */
 
 import OrangeDesignSystem
 import SwiftUI
@@ -62,7 +47,7 @@ struct SliderVariant: View {
                 
                 if model.stepped {
                     ODSSlider(value: $value, in: range, step: 0.5) {
-                        Text("Volume")
+                        Text("screens.guidelines.sliders.sample.volume")
                     } minimumValueLabel: {
                         SliderLabel(show: model.showSideIcons, systemName: "speaker.wave.1.fill")
                     } maximumValueLabel: {
@@ -72,7 +57,7 @@ struct SliderVariant: View {
                     }
                 } else {
                     ODSSlider(value: $value, in: range) {
-                        Text("Volume")
+                        Text("screens.guidelines.sliders.sample.volume")
                     } minimumValueLabel: {
                         SliderLabel(show: model.showSideIcons, systemName: "speaker.wave.1.fill")
                     } maximumValueLabel: {
@@ -130,9 +115,9 @@ struct SliderVariantOptions: View {
 
     var body: some View {
         VStack(spacing: ODSSpacing.m) {
-            Toggle("Side icons", isOn: $model.showSideIcons)
-            Toggle("Display value", isOn: $model.showValue)
-            Toggle("Stepped (0.5)", isOn: $model.stepped)
+            Toggle("screens.guidelines.sliders.sample.side_icons", isOn: $model.showSideIcons)
+            Toggle("screens.guidelines.sliders.sample.display_value", isOn: $model.showValue)
+            Toggle("screens.guidelines.sliders.sample.stepped", isOn: $model.stepped)
         }
         .odsFont(.bodyRegular)
         .padding(.vertical, ODSSpacing.m)
