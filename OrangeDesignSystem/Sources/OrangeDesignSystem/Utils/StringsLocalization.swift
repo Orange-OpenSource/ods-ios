f230105d0708c08338ef1d74cba62839d4088866
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2023-10-26 16:48:07 +0200

    refactor(#466): review - better String extension (lib)
    - Only one extension in lib, public, instead of one in lib and one in app
    - Implicit management of Bundle (.main or .ods)
    - A bit of fun in the source code
    - More unit tests
    
    [Reviewed offline with Ludovic PINEL]
    
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ OrangeDesignSystem/Sources/OrangeDesignSystem/Utils/StringsLocalization.swift
@@ -0,0 +1,104 @@
+/*
+ * Software Name: Orange Design System (iOS)
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2023 Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license.
+ */
+
+import Foundation
+
+// ========================================
+// MARK: - Operators for smaller code lines
+// ========================================
+
+prefix operator °°
+infix operator <-
+
+// ========================
+// MARK: - String extension
+// ========================
+
+extension String {
+
+    // =============
+    // MARK: - Sugar
+    // =============
+
+    /// Ensures the string you handle has been localized,
+    /// e.g. when you store the string as `String` and not implicitly as `LocalizedString` (like done in Views, sometimes the cast fails).
+    /// Could also bring some fun in the source code, remember to keep it clear of course.
+    /// ʕ•́ᴥ•̀ʔっ You should not use that.
+    public var 🌐: String {
+        localized()
+    }
+
+    /// A prefix operator to return the localized string of the given `String` seen as a wording key.
+    /// Could also bring some sequins in the source code 💫, remember to keep it clear of course.
+    /// ʕ•́ᴥ•̀ʔっ You should not use that.
+    /// - Parameter wordingKey: Some key expected in your localizables
+    /// - Returns String: The localized string value
+    public static prefix func °° (_ wordingKey: String) -> String {
+        wordingKey.localized()
+    }
+
+    // =================
+    // MARK: - Localized
+    // =================
+
+    // swiftlint:disable nslocalizedstring_key
+    /// Returns the localized result string using `self` as key.
+    /// Will first look for Bundle.main localizables, then as a last fallback will look in ODS library bundle.
+    /// - Returns String: The conversion of `self` as `NSLocalizedString`
+    public func localized() -> String {
+        // First try: Get wording from Bundle.main (default) for app localizables
+        let expectedAppWording = NSLocalizedString(self, bundle: Bundle.main, comment: "")
+        if expectedAppWording != self {
+            return expectedAppWording
+        }
+
+        // Second try: Get wording from Bundle.os for SDK localizables
+        return NSLocalizedString(self, bundle: Bundle.ods, comment: "")
+    }
+    // swiftlint:enable nslocalizedstring_key
+
+    /// Returns a localized String using `self` as wording key expected to be in localizables.
+    /// Will then inject as `String` the given `argument` inside the parameterized string.
+    /// Will first look for Bundle.main localizables, then as a last fallback will look in ODS library bundle.
+    /// - Parameter argument: The unique value to insert in the localized string with `self` as key
+    /// - Returns: The localized `String`
+    public func localized(with argument: CVarArg) -> String {
+        String(format: localized(), argument)
+    }
+
+    /// Returns a localized String using `self` as wording key expected to be in localizables.
+    /// Will then inject as `String` the given `argument` objects inside the parameterized string.
+    /// Will first look for Bundle.main localizables, then as a last fallback will look in ODS library bundle.
+    /// - Parameter arguments: An array of values to insert in the localized string with `self` as key
+    /// - Returns: The localized `String`
+    public func localized(with arguments: [CVarArg]) -> String {
+        String(format: localized(), arguments: arguments)
+    }
+
+    // =============================
+    // MARK: - Convenience operators
+    // =============================
+
+    /// Just an operator to inject as `String` one argument in a localizable
+    /// - Parameters:
+    ///    - lhs: The localizable value to update
+    ///    - rhs: The value as String to inject in `lhs`
+    /// - Returns String: The final result
+    public static func <- (lhs: String, rhs: String) -> String {
+        return String(format: lhs.localized(), rhs)
+    }
+
+    /// Just an operator to inject as `String` array some arguments in a localizable
+    /// - Parameters:
+    ///    - lhs: The localizable value to update
+    ///    - rhs: The values as `[String] to inject in `lhs`
+    /// - Returns String: The final result
+    public static func <- (lhs: String, rhs: [String]) -> String {
+        String(format: lhs.localized(), arguments: rhs)
+    }
+}
---
+++ OrangeDesignSystemDemo/StringLocalizationTests.swift
@@ -0,0 +1,321 @@
+/*
+ * Software Name: Orange Design System (iOS)
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2023 Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license.
+ */
+
+import Foundation
+@testable import OrangeDesignSystem
+import XCTest
+
+final class StringTests: XCTestCase {
+    
+    // ================
+    // MARK: - 🌐 sugar
+    // ================
+
+    func test🌐WithCommonWording() {
+        // Given
+        let someWording = "Valar Morghulis"
+        // When
+        let result = someWording.🌐
+        // Then
+        XCTAssertTrue(result == someWording, "Value under test is '\(result)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func test🌐WithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey.🌐
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.guidelines.list.details" = "Détails"
+    func test🌐WithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.guidelines.list.details"
+        // When
+        let result = someAppLocalizable.🌐
+        // Then
+        XCTAssertTrue(result == "Détails", "Value under test is '\(result)'")
+    }
+
+    // Expect to have in lib app Localizables entry "modules.about.terms_of_service.title" = "Terms of Service"
+    func test🌐WithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.terms_of_service.title"
+        // When
+        let result = someLibLocalizable.🌐
+        // Then
+        XCTAssertTrue(result == "Terms of Service", "Value under test is '\(result)'")
+    }
+
+    // ===================
+    // MARK: - °° operator
+    // ===================
+
+    func testOOWithCommonWording() {
+        // Given
+        let someWording = "Valar Morghulis"
+        // When
+        let result = °°someWording
+        // Then
+        XCTAssertTrue(result == someWording, "Value under test is '\(result)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testOOWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = °°someFakeKey
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.guidelines.list.details" = "Détails"
+    func testOOWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.guidelines.list.details"
+        // When
+        let result = °°someAppLocalizable
+        // Then
+        XCTAssertTrue(result == "Détails", "Value under test is '\(result)'")
+    }
+
+    // Expect to have in lib app Localizables entry "modules.about.terms_of_service.title" = "Terms of Service"
+    func testOOWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.terms_of_service.title"
+        // When
+        let result = °°someLibLocalizable
+        // Then
+        XCTAssertTrue(result == "Terms of Service", "Value under test is '\(result)'")
+    }
+    
+    // ===================
+    // MARK: - localized()
+    // ===================
+
+    func testLocalizedWithCommonWording() {
+        // Given
+        let someWording = "Valar Morghulis"
+        // When
+        let result = someWording.localized()
+        // Then
+        XCTAssertTrue(result == someWording, "Value under test is '\(result)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testLocalizedWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey.localized()
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.guidelines.list.details" = "Détails"
+    func testLocalizedWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.guidelines.list.details"
+        // When
+        let result = someAppLocalizable.localized()
+        // Then
+        XCTAssertTrue(result == "Détails", "Value under test is '\(result)'")
+    }
+
+    // Expect to have in lib app Localizables entry "modules.about.terms_of_service.title" = "Terms of Service"
+    func testLocalizedWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.terms_of_service.title"
+        // When
+        let result = someLibLocalizable.localized()
+        // Then
+        XCTAssertTrue(result == "Terms of Service", "Value under test is '\(result)'")
+    }
+
+    // ========================
+    // MARK: - localized(with:)
+    // ========================
+
+    func testLocalizedWithOneArgWithCommonWording() {
+        // Given
+        let someWording = "Valar Morghulis"
+        // When
+        let result = someWording.localized(with: "No one")
+        // Then
+        XCTAssertTrue(result == someWording, "Value under test is '\(result)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testLocalizedWithOneArgWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey.localized(with: "Bahak")
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.guidelines.lists.variant.clicked" = "%@ est tappé"
+    func testLocalizedWithOneArgWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.guidelines.lists.variant.clicked"
+        // When
+        let result = someAppLocalizable.localized(with: "Kenny")
+        // Then
+        XCTAssertTrue(result == "Kenny est tappé", "Value under test is '\(result)'")
+    }
+
+    // Expect to have in lib app Localizables entry "modules.about.app_information.full_version_text" = "Version %@"
+    func testLocalizedWithOneArgWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.app_information.full_version_text"
+        // When
+        let result = someLibLocalizable.localized(with: "T101")
+        // Then
+        XCTAssertTrue(result == "Version T101", "Value under test is '\(result)'")
+    }
+    
+    // ==========================
+    // MARK: - localized(with:[])
+    // ==========================
+    
+    func testLocalizedWithSeveralArgsWithCommonWording() {
+        // Given
+        let someWording = "Valar Morghulis"
+        // When
+        let result = someWording.localized(with: ["No", "one"])
+        // Then
+        XCTAssertTrue(result == someWording, "Value under test is '\(result)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testLocalizedWithSeveralArgsWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey.localized(with: ["Bahak", "Alpha"])
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.about.file_load_failure" = "Impossible de charger le fichier %@.%@"
+    func testLocalizedWithSeveralArgsWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.about.file_load_failure"
+        // When
+        let result = someAppLocalizable.localized(with: ["eps3.8_stage3", "torrent"])
+        // Then
+        XCTAssertTrue(result == "Impossible de charger le fichier eps3.8_stage3.torrent", "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in lib app Localizables entry "modules.about.app_information.full_enriched_version_text" = "Version %@ (build %@)"
+    func testLocalizedWithSeveralArgWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.app_information.full_enriched_version_text"
+        // When
+        let result = someLibLocalizable.localized(with: ["Normandy", "SR-2"])
+        // Then
+        XCTAssertTrue(result == "Version Normandy (build SR-2)", "Value under test is '\(result)'")
+    }
+    
+    // ===================
+    // MARK: - <- operator
+    // ===================
+    
+    func testArrowWithOneArg() {
+        // Given
+        let someLocalizable = "Émiettez votre arsenic dans %@"
+        let someArg = "un verre de narcotique"
+        
+        // When
+        let completeSentence = someLocalizable <- someArg
+        
+        // Then
+        XCTAssertTrue(completeSentence == "Émiettez votre arsenic dans un verre de narcotique", "Value under test is '\(completeSentence)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testArrowWithOneArgWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey <- "Bahak"
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.guidelines.lists.variant.clicked" = "%@ est tappé";
+    func testArrowWithOneArgWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.guidelines.lists.variant.clicked"
+        // When
+        let result = someAppLocalizable <- "Kenny"
+        // Then
+        XCTAssertTrue(result == "Kenny est tappé", "Value under test is '\(result)'")
+    }
+
+    // Expect to have in lib app Localizables entry "modules.about.app_information.full_version_text" = "Version %@"
+    func testArrowWithOneArgWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.app_information.full_version_text"
+        // When
+        let result = someLibLocalizable <- "T101"
+        // Then
+        XCTAssertTrue(result == "Version T101", "Value under test is '\(result)'")
+    }
+    
+    func testArrowWithSeveralArgs() {
+        // Given
+        let someLocalizable = """
+        Pour adoucir le mélange, \
+        pressez %1$@ quartiers d'orange. \
+        Ho ho, je vais en mettre %2$@
+        """
+        let someArgs = ["trois", "un seul"]
+        
+        // When
+        let completeSentence = someLocalizable <- someArgs
+        
+        // Then
+        XCTAssertTrue(completeSentence == "Pour adoucir le mélange, pressez trois quartiers d'orange. Ho ho, je vais en mettre un seul", "Value under test is '\(completeSentence)'")
+    }
+    
+    // Expect the key "jack_miranda_liara_tali" is not available in any Localizables
+    func testArrowWithSeveralArgsWithMissingKey() {
+        // Given
+        let someFakeKey = "jack_miranda_liara_tali"
+        // When
+        let result = someFakeKey <- ["Bahak", "Alpha"]
+        // Then
+        XCTAssertTrue(result == someFakeKey, "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in demo app for "fr" Localizables entry "screens.about.file_load_failure" = "Impossible de charger le fichier %@.%@"
+    func testArrowWithSeveralArgsWithAppWording() {
+        // Given
+        let someAppLocalizable = "screens.about.file_load_failure"
+        // When
+        let result = someAppLocalizable <- ["eps3.8_stage3", "torrent"]
+        // Then
+        XCTAssertTrue(result == "Impossible de charger le fichier eps3.8_stage3.torrent", "Value under test is '\(result)'")
+    }
+    
+    // Expect to have in lib app Localizables entry "modules.about.app_information.full_enriched_version_text" = "Version %@ (build %@)"
+    func testArrowWithSeveralArgsWithLibWording() {
+        // Given
+        let someLibLocalizable = "modules.about.app_information.full_enriched_version_text"
+        // When
+        let result = someLibLocalizable <- ["Normandy", "SR-2"]
+        // Then
+        XCTAssertTrue(result == "Version Normandy (build SR-2)", "Value under test is '\(result)'")
+    }
+}
