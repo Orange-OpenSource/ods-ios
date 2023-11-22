034d7486620dbdf5b4a2c4f4a9b4a8aa7ab6c80d
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2023-11-22 19:06:48 +0100

    feat(#537): add new cache for data formatter with using date format
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OrangeDesignSystem/Sources/OrangeDesignSystem/Utils/DateFormatterCache.swift
+++ OrangeDesignSystem/Sources/OrangeDesignSystem/Utils/DateFormatterCache.swift
@@ -9,19 +9,30 @@
 import Foundation
 
 /// Because the instanciations of `DateFormatter` can be costly, helps to store such objects according to their configuration
+/// See https://sarunw.com/posts/how-expensive-is-dateformatter/
 final class DateFormatterCache {
 
-    /// The cache of formatters depending to some configuration they must have
-    private var cache: Cache<DateFormatterCache.Configuration, DateFormatter>
+    // MARK: - Properties
+
+    /// A cache of formatters depending to some configuration they must have (locale, date style and time style)
+    private var localeDateTimeCache: Cache<DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter>
+
+    /// A cache of formatters depending to some configuration they must have (date format)
+    private var dateFormatCache: Cache<DateFormatterCache.DateFormatConfiguration, DateFormatter>
+
+    // MARK: - Singleton definition
 
     /// The singleton
     static let shared = DateFormatterCache()
 
     private init() {
-        cache = Cache<DateFormatterCache.Configuration, DateFormatter>()
+        localeDateTimeCache = Cache<DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter>()
+        dateFormatCache = Cache<DateFormatterCache.DateFormatConfiguration, DateFormatter>()
     }
 
-    struct Configuration: Hashable {
+    // MARK: - Locale Date Time Configuration
+
+    struct LocaleDateTimeConfiguration: Hashable {
         let locale: Locale
         let dateStyle: DateFormatter.Style
         let timeStyle: DateFormatter.Style
@@ -29,19 +40,48 @@ final class DateFormatterCache {
 
     /// - Returns: The `DateFormatter` which has the given arguments in configuration, or `nil` of none
     func formatter(for locale: Locale, dateStyle: DateFormatter.Style, timeStyle: DateFormatter.Style) -> DateFormatter? {
-        cache[Configuration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)]
+        localeDateTimeCache[LocaleDateTimeConfiguration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)]
     }
 
-    /// Stores in the cahe the given `DateFormatter` and overrides the prveious value if it exists
-    /// - Parameter formatter: The `DateFormatter` to store according to its locale, date style and time style
-    func store(formatter: DateFormatter) {
-        cache[Configuration(locale: formatter.locale,
-                            dateStyle: formatter.dateStyle,
-                            timeStyle: formatter.timeStyle)] = formatter
+    // MARK: - Date Format Confguration
+
+    struct DateFormatConfiguration: Hashable {
+        let dateFormat: String
     }
 
-    /// Deletes all the entries store din the cache
+    /// - Returns: The `DateFormatter` which has the given argument in configuration, or `nil` of none
+    func formatter(for dateFormat: String) -> DateFormatter? {
+        dateFormatCache[DateFormatConfiguration(dateFormat: dateFormat)]
+    }
+
+    // MARK: - Store
+
+    /// To specifiy the type of cache the date formatters must be stored
+    enum CacheType {
+        case localeDateTimeCache
+        case dateFormatCache
+    }
+
+    /// Stores in the cache the given `DateFormatter` and overrides the previous value if it exists
+    /// - Parameters:
+    ///    - formatter: The `DateFormatter` to store using the specified cache type
+    ///    - cacheType: The type of cache to use defining the configuration of the date formatter as key
+    func store(formatter: DateFormatter, using cacheType: CacheType) {
+        switch cacheType {
+        case .localeDateTimeCache:
+            localeDateTimeCache[LocaleDateTimeConfiguration(locale: formatter.locale,
+                                                            dateStyle: formatter.dateStyle,
+                                                            timeStyle: formatter.timeStyle)] = formatter
+        case .dateFormatCache:
+            dateFormatCache[DateFormatConfiguration(dateFormat: formatter.dateFormat)] = formatter
+        }
+    }
+
+    // MARK: - Flush
+
+    /// Deletes all the entries stored in the caches
     func flush() {
-        cache.removeAllValues()
+        localeDateTimeCache.removeAllValues()
+        dateFormatCache.removeAllValues()
     }
 }
--- OrangeDesignSystemDemo/OrangeDesignSystemDemoTests/CacheTests.swift
+++ OrangeDesignSystemDemo/OrangeDesignSystemDemoTests/CacheTests.swift
@@ -11,11 +11,11 @@ import XCTest
 
 class CacheTests: XCTestCase {
 
-    func testSetValueSucceeds_dateFormatter() {
+    func testSetValueSucceeds_dateFormatter_localeDateTimeConfiguration() {
         // Given
-        let (key1, value1) = firstKeyValuePair()
-        let (key2, value2) = secondKeyValuePair()
-        let cache = Cache<DateFormatterCache.Configuration, DateFormatter>()
+        let (key1, value1) = firstKeyValuePairForLocaleDateTimeConfiguration()
+        let (key2, value2) = secondKeyValuePairForLocaleDateTimeConfiguration()
+        let cache = Cache<DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter>()
 
         // When
         cache[key1] = value1
@@ -28,11 +28,28 @@ class CacheTests: XCTestCase {
         XCTAssertEqual(value2, cache.value(forKey: key2))
     }
 
-    func testRemoveValueSucceeds_dateFormatter() {
+    func testSetValueSucceeds_dateFormatter_dateFormatConfiguration() {
         // Given
-        let (key1, value1) = firstKeyValuePair()
-        let (key2, value2) = secondKeyValuePair()
-        let cache = Cache<DateFormatterCache.Configuration, DateFormatter>()
+        let (key1, value1) = firstKeyValuePairForDateFormatConfiguration()
+        let (key2, value2) = secondKeyValuePairForDateFormatConfiguration()
+        let cache = Cache<DateFormatterCache.DateFormatConfiguration, DateFormatter>()
+
+        // When
+        cache[key1] = value1
+        cache.setValue(value2, forKey: key2)
+
+        // Then
+        XCTAssertEqual(value1, cache[key1])
+        XCTAssertEqual(value1, cache.value(forKey: key1))
+        XCTAssertEqual(value2, cache[key2])
+        XCTAssertEqual(value2, cache.value(forKey: key2))
+    }
+
+    func testRemoveValueSucceeds_dateFormatter_localeDateTimeConfiguration() {
+        // Given
+        let (key1, value1) = firstKeyValuePairForLocaleDateTimeConfiguration()
+        let (key2, value2) = secondKeyValuePairForLocaleDateTimeConfiguration()
+        let cache = Cache<DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter>()
         cache[key1] = value1
         cache[key2] = value2
 
@@ -45,11 +62,44 @@ class CacheTests: XCTestCase {
         XCTAssertNil(cache[key2])
     }
 
-    func testRemoveAllValuesSucceeds_dateFormatter() {
+    func testRemoveValueSucceeds_dateFormatter_dateFormatConfiguration() {
         // Given
-        let (key1, value1) = firstKeyValuePair()
-        let (key2, value2) = secondKeyValuePair()
-        let cache = Cache<DateFormatterCache.Configuration, DateFormatter>()
+        let (key1, value1) = firstKeyValuePairForDateFormatConfiguration()
+        let (key2, value2) = secondKeyValuePairForDateFormatConfiguration()
+        let cache = Cache<DateFormatterCache.DateFormatConfiguration, DateFormatter>()
+        cache[key1] = value1
+        cache[key2] = value2
+
+        // When
+        cache[key1] = nil
+        cache.removeValue(forKey: key2)
+
+        // Then
+        XCTAssertNil(cache[key1])
+        XCTAssertNil(cache[key2])
+    }
+
+    func testRemoveAllValuesSucceeds_dateFormatter_localeDateTimeConfiguration() {
+        // Given
+        let (key1, value1) = firstKeyValuePairForLocaleDateTimeConfiguration()
+        let (key2, value2) = secondKeyValuePairForLocaleDateTimeConfiguration()
+        let cache = Cache<DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter>()
+        cache[key1] = value1
+        cache[key2] = value2
+
+        // When
+        cache.removeAllValues()
+
+        // Then
+        XCTAssertNil(cache[key1])
+        XCTAssertNil(cache[key2])
+    }
+
+    func testRemoveAllValuesSucceeds_dateFormatter_dateFormatConfiguration() {
+        // Given
+        let (key1, value1) = firstKeyValuePairForDateFormatConfiguration()
+        let (key2, value2) = secondKeyValuePairForDateFormatConfiguration()
+        let cache = Cache<DateFormatterCache.DateFormatConfiguration, DateFormatter>()
         cache[key1] = value1
         cache[key2] = value2
 
@@ -65,24 +115,42 @@ class CacheTests: XCTestCase {
     // MARK: - Helpers
     // ===============
 
-    private func firstKeyValuePair() -> (DateFormatterCache.Configuration, DateFormatter) {
+    private func firstKeyValuePairForLocaleDateTimeConfiguration() -> (DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter) {
         let locale = Locale(identifier: "fr")
         let dateStyle = DateFormatter.Style.short
         let timeStyle = DateFormatter.Style.none
 
-        let key = DateFormatterCache.Configuration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)
+        let key = DateFormatterCache.LocaleDateTimeConfiguration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)
         let value = DateFormatter.formatter(for: locale, dateStyle: dateStyle, timeStyle: timeStyle)
 
         return (key, value)
     }
 
-    private func secondKeyValuePair() -> (DateFormatterCache.Configuration, DateFormatter) {
+    private func secondKeyValuePairForLocaleDateTimeConfiguration() -> (DateFormatterCache.LocaleDateTimeConfiguration, DateFormatter) {
         let locale = Locale(identifier: "pl")
         let dateStyle = DateFormatter.Style.medium
         let timeStyle = DateFormatter.Style.full
 
-        let key = DateFormatterCache.Configuration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)
+        let key = DateFormatterCache.LocaleDateTimeConfiguration(locale: locale, dateStyle: dateStyle, timeStyle: timeStyle)
         let value = DateFormatter.formatter(for: locale, dateStyle: dateStyle, timeStyle: timeStyle)
+
+        return (key, value)
+    }
+
+    private func firstKeyValuePairForDateFormatConfiguration() -> (DateFormatterCache.DateFormatConfiguration, DateFormatter) {
+        let dateFormat = "yyyy-MM-dd"
+
+        let key = DateFormatterCache.DateFormatConfiguration(dateFormat: dateFormat)
+        let value = DateFormatter.formatter(for: dateFormat)
+
+        return (key, value)
+    }
+
+    private func secondKeyValuePairForDateFormatConfiguration() -> (DateFormatterCache.DateFormatConfiguration, DateFormatter) {
+        let dateFormat = "dd-mm-yy"
+
+        let key = DateFormatterCache.DateFormatConfiguration(dateFormat: dateFormat)
+        let value = DateFormatter.formatter(for: dateFormat)
 
         return (key, value)
     }
